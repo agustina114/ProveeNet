b06ef10f902bd4758bd9948454cc21311ea41832
agustina Poblete Alvarez <agustinapoblete058@gmail.com>
2025-11-07 16:00:27 -0300

    primer commit

---
+++ app/src/main/java/com/proveenet/proveenet/MainActivity.java
@@ -0,0 +1,14 @@
+package com.proveenet.proveenet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
